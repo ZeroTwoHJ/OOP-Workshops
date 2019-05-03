d1d0527282f773890da7749692ca3fcb7124603c
cornel <cornel.barna@senecacollege.ca>
2019-05-03 15:25:23 -0400

    Reset for Summer 2019 term (2194).

--- WS04/in-lab/Traveler.h
+++
@@ -1,9 +0,0 @@
-// TODO: add file header comments here
-
-// TODO: add header file guard here
-
-// TODO: declare your namespace here
-
-	// TODO: define the constants here
-
-    // TODO: define the Traveler class here
